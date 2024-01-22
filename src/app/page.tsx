0c4dd684f7a92c2fd4e3c3cf3e8f25089f2e8bec
ayobami.ajakaye <ayobami.ajakaye@cecureintel.com>
2024-01-22 16:31:24 +0100

    feature: productsection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import HeroCards from "@/components/HeroCards";
 import ProductSection from "@/components/ProductSection";
 import Image from "next/image";
+import feat1 from "@/assets/images/bx_bxs-book-reader.svg";
+import feat2 from "@/assets/images/carbon_book.svg";
+import feat3 from "@/assets/images/uil_arrow-growth.svg";
 
 export default function Home() {
   return (
@@ -20,6 +23,84 @@ export default function Home() {
           </p>
 
           <ProductSection showButton />
+        </div>
+      </section>
+
+      {/*  */}
+      <section className="container mx-auto">
+        <div className="w-full py-20 flex-col justify-center pb-6 items-center gap-2.5 inline-flex">
+          <p className="hidden lg:block text-center text-neutral-500 text-xl font-normal">
+            Featured Products
+          </p>
+          <h3 className="w-[239px] md:w-full text-center text-slate-800 text-2xl font-bold">
+            BESTSELLER PRODUCTS
+          </h3>
+          <p className="w-[261px] md:w-full text-center text-neutral-500 text-sm font-normal">
+            Problems trying to resolve the conflict between{" "}
+          </p>
+
+          <div className="flex flex-col md:flex-row">
+            <div className="md:w-[328px] flex-col justify-start items-center inline-flex">
+              <div className="px-10 py-[35px] flex-col justify-start items-center gap-5 flex">
+                <Image
+                  src={feat1}
+                  alt="feat 1"
+                  className="object-cover object-center"
+                />
+                <div className="w-[235px] text-center text-slate-800 text-2xl font-bold leading-loose tracking-tight">
+                  Easy Wins
+                </div>
+                <div className="w-[225px] text-center text-neutral-500 text-sm font-normal leading-tight tracking-tight">
+                  Get your best looking smile now!
+                </div>
+              </div>
+            </div>
+
+            <div className="md:w-[328px] flex-col justify-start items-center inline-flex">
+              <div className="px-10 py-[35px] flex-col justify-start items-center gap-5 flex">
+                <Image
+                  src={feat2}
+                  alt="feat 1"
+                  className="object-cover object-center"
+                />
+                <div className="w-[235px] text-center text-slate-800 text-2xl font-bold leading-loose tracking-tight">
+                  Easy Wins
+                </div>
+                <div className="w-[225px] text-center text-neutral-500 text-sm font-normal leading-tight tracking-tight">
+                  Get your best looking smile now!
+                </div>
+              </div>
+            </div>
+
+            <div className="md:w-[328px] flex-col justify-start items-center inline-flex">
+              <div className="px-10 py-[35px] flex-col justify-start items-center gap-5 flex">
+                <Image
+                  src={feat3}
+                  alt="feat 1"
+                  className="object-cover object-center"
+                />
+                <div className="w-[235px] text-center text-slate-800 text-2xl font-bold leading-loose tracking-tight">
+                  Easy Wins
+                </div>
+                <div className="w-[225px] text-center text-neutral-500 text-sm font-normal leading-tight tracking-tight">
+                  Get your best looking smile now!
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* FEATURED POSTS */}
+
+      <section className="container py-20 mx-auto">
+        <div className="lg:w-full h-[134px] text-center ">
+          <p className="text-center text-sky-500 text-sm font-bold leading-normal tracking-tight">
+            Practice Advice
+          </p>
+          <h3 className="lg:w-full text-center text-slate-800 text-[40px] font-bold leading-[50px] tracking-tight">
+            Featured Products
+          </h3>
         </div>
       </section>
     </div>
--- src/components/ProductSection.tsx
+++ src/components/ProductSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Product from "./Product";
+import Button from "@mui/material/Button";
 
 interface ProductSection {
   showButton: boolean;
@@ -8,11 +9,24 @@ interface ProductSection {
 const ProductSection = ({ showButton }: ProductSection) => {
   const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
-    <div className="flex py-6 flex-col md:flex-row md:flex-wrap md:gap-[0px] lg:gap[30px]">
-      {arr.map((x) => (
-        <Product key={x} />
-      ))}
-    </div>
+    <>
+      <div className="flex py-6 flex-col md:flex-row md:flex-wrap md:gap-[0px] lg:gap[30px]">
+        {arr.map((x) => (
+          <Product key={x} />
+        ))}
+      </div>
+
+      <div className=" px-10 py-[15px] flex-col justify-start items-center gap-2.5 inline-flex">
+        <Button
+          variant="outlined"
+          size="large"
+          fullWidth
+          sx={{ height: "52px" }}
+        >
+          LOAD MORE PRODUCTS
+        </Button>
+      </div>
+    </>
   );
 };
 
